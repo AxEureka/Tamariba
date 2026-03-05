edc1018f5d888909410b5a3a6b84950d3b5e24fb
AxEureka <lilium813@gmail.com>
2026-03-05 14:18:04 +0900

    Update quiz-ui.js

--- static/js/quiz/quiz-ui.js
+++ static/js/quiz/quiz-ui.js
@@ -1,1 +1,13 @@
-console.log("quiz-ui loaded");
+function startTestQuiz() {
+
+  fetch(`/room/${roomId}/quiz/start`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name: myName,
+      question: "日本の首都は？",
+      choices: ["東京", "大阪", "京都", "福岡"]
+    })
+  });
+
+}
