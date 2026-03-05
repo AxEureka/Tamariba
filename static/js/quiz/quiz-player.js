17b251c6bfc62929c56aaddfca8b65735fd60564
AxEureka <lilium813@gmail.com>
2026-03-05 16:24:07 +0900

    Update quiz-player.js

--- static/js/quiz/quiz-player.js
+++ static/js/quiz/quiz-player.js
@@ -1,22 +1,40 @@
-// quiz-player.js
 import { createQuestionUI, showCorrectAnswer } from "./quiz-ui.js";
 
 export function startQuizPlayer(socket, container) {
 
-  socket.onmessage = (event) => {
+  socket.addEventListener("message", (event) => {
+
     const data = JSON.parse(event.data);
 
+    // 問題表示
     if(data.type === "new_question") {
-      createQuestionUI(container, data.question, data.choices, (choiceIndex)=>{
-        socket.send(JSON.stringify({
-          type: "answer",
-          choice: choiceIndex
-        }));
-      });
+
+      createQuestionUI(
+        container,
+        data.question,
+        data.choices,
+        (choiceIndex)=>{
+
+          socket.send(JSON.stringify({
+            type: "answer",
+            choice: choiceIndex
+          }));
+
+        }
+      );
+
     }
 
+    // 正解発表
     if(data.type === "show_answer") {
-      showCorrectAnswer(container, data.correct, ["A","B","C","D"]);
+
+      showCorrectAnswer(
+        container,
+        data.correct
+      );
+
     }
-  };
+
+  });
+
 }
