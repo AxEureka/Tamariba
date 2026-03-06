d852f76b8619ea0e528c8bc2d6a9e2efec3b9272
AxEureka <lilium813@gmail.com>
2026-03-06 13:26:06 +0900

    Update quiz-host.js

--- static/js/quiz/quiz-host.js
+++ static/js/quiz/quiz-host.js
@@ -1,36 +1,27 @@
-// quiz-host.js
-
 let socket;
 let votes = [0,0,0,0];
 let correctAnswer = 0;
 let players = {};
 
-export function initQuizHost(ws) {
-
+// ここを startQuizHost に変更
+export function startQuizHost(ws, container) {
   socket = ws;
 
   socket.addEventListener("message", e => {
-
     const data = JSON.parse(e.data);
 
     if (data.type === "quiz_answer") {
-
       const a = data.answer;
-
       if (a === undefined) return;
-
       votes[a]++;
-
       broadcastVotes();
-
     }
-
   });
 
+  // container は必要なら使う
 }
 
 export function sendQuestion(question, choices, answer) {
-
   votes = [0,0,0,0];
   correctAnswer = answer;
 
@@ -39,23 +30,18 @@ export function sendQuestion(question, choices, answer) {
     question: question,
     choices: choices
   }));
-
 }
 
 export function revealAnswer() {
-
   socket.send(JSON.stringify({
     type: "quiz_correct",
     answer: correctAnswer
   }));
-
 }
 
 function broadcastVotes() {
-
   socket.send(JSON.stringify({
     type: "quiz_votes",
     votes: votes
   }));
-
 }
