c8927d750ed656466e0cf04310240c5a7c0d68f2
AxEureka <lilium813@gmail.com>
2026-03-09 16:48:42 +0900

    Update quiz-player.js

--- static/js/quiz/quiz-player.js
+++ static/js/quiz/quiz-player.js
@@ -1,46 +1,60 @@
-// quiz-player.js
+// quiz-host.js
 
-import {
-createQuestionUI,
-updateGraph,
-showCorrectAnswer
-} from "./quiz-ui.js";
-
-let container;
 let socket;
-let choices = [];
+let votes = [0,0,0,0];
+let correctAnswer = 0;
 
-export function startQuizPlayer(ws, uiContainer) {
+export function startQuizHost(ws, container){
 
 socket = ws;
-container = uiContainer;
 
-socket.addEventListener("message", e => {
+container.innerHTML = `
+<div id="quiz-host-ui">
+
+<h2>クイズ出題</h2>
+
+<input id="quiz-question" placeholder="問題文"><br><br>
+
+<input class="quiz-choice" placeholder="選択肢1"><br>
+<input class="quiz-choice" placeholder="選択肢2"><br>
+<input class="quiz-choice" placeholder="選択肢3"><br>
+<input class="quiz-choice" placeholder="選択肢4"><br><br>
+
+正解:
+<select id="quiz-answer">
+<option value="0">1</option>
+<option value="1">2</option>
+<option value="2">3</option>
+<option value="3">4</option>
+</select>
+
+<br><br>
+
+<button id="send-question">出題</button>
+<button id="reveal-answer">正解発表</button>
+
+<h3>投票結果</h3>
+<div id="vote-result"></div>
+
+</div>
+`;
+
+document.getElementById("send-question").onclick = sendQuestion;
+document.getElementById("reveal-answer").onclick = revealAnswer;
+
+socket.addEventListener("message", e=>{
 
 const data = JSON.parse(e.data);
 
-if (data.type === "quiz_question") {
+if(data.type === "quiz_answer"){
 
-choices = data.choices;
+const a = data.answer;
+if(a === undefined) return;
 
-createQuestionUI(
-container,
-data.question,
-choices,
-sendAnswer
-);
+votes[a]++;
 
-}
-
-if (data.type === "quiz_votes") {
-
-updateGraph(data.votes, choices);
-
-}
-
-if (data.type === "quiz_correct") {
-
-showCorrectAnswer(data.answer);
+broadcastVotes();
+updateVotes();
 
 }
 
@@ -48,11 +62,57 @@ showCorrectAnswer(data.answer);
 
 }
 
-function sendAnswer(index) {
+function sendQuestion(){
+
+const q = document.getElementById("quiz-question").value;
+
+const choices = [...document.querySelectorAll(".quiz-choice")]
+.map(i=>i.value);
+
+const answer = parseInt(
+document.getElementById("quiz-answer").value
+);
+
+votes = [0,0,0,0];
+correctAnswer = answer;
 
 socket.send(JSON.stringify({
-type: "quiz_answer",
-answer: index
+type:"quiz_question",
+question:q,
+choices:choices
 }));
+
+updateVotes();
+
+}
+
+function revealAnswer(){
+
+socket.send(JSON.stringify({
+type:"quiz_correct",
+answer:correctAnswer
+}));
+
+}
+
+function broadcastVotes(){
+
+socket.send(JSON.stringify({
+type:"quiz_votes",
+votes:votes
+}));
+
+}
+
+function updateVotes(){
+
+const box = document.getElementById("vote-result");
+if(!box) return;
+
+box.innerHTML =
+"1: "+votes[0]+"票<br>"+
+"2: "+votes[1]+"票<br>"+
+"3: "+votes[2]+"票<br>"+
+"4: "+votes[3]+"票";
 
 }
