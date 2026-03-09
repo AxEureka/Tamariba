ce34eb3144e3268ff06d5717725abf0eeef7a146
AxEureka <lilium813@gmail.com>
2026-03-09 10:23:19 +0900

    Update quiz-player.js

--- static/js/quiz/quiz-player.js
+++ static/js/quiz/quiz-player.js
@@ -1,9 +1,9 @@
 // quiz-player.js
 
 import {
-  createQuestionUI,
-  updateGraph,
-  showCorrectAnswer
+createQuestionUI,
+updateGraph,
+showCorrectAnswer
 } from "./quiz-ui.js";
 
 let container;
@@ -13,47 +13,49 @@ let choices = [];
 // ★ init → start に変更しただけ
 export function startQuizPlayer(ws, uiContainer) {
 
-  socket = ws;
-  container = uiContainer;
+socket = ws;
+container = uiContainer;
 
-  socket.addEventListener("message", e => {
+socket.onmessage = e => {
 
-    const data = JSON.parse(e.data);
+```
+const data = JSON.parse(e.data);
 
-    if (data.type === "quiz_question") {
+if (data.type === "quiz_question") {
 
-      choices = data.choices;
+  choices = data.choices;
 
-      createQuestionUI(
-        container,
-        data.question,
-        choices,
-        sendAnswer
-      );
+  createQuestionUI(
+    container,
+    data.question,
+    choices,
+    sendAnswer
+  );
 
-    }
+}
 
-    if (data.type === "quiz_votes") {
+if (data.type === "quiz_votes") {
 
-      updateGraph(data.votes, choices);
+  updateGraph(data.votes, choices);
 
-    }
+}
 
-    if (data.type === "quiz_correct") {
+if (data.type === "quiz_correct") {
 
-      showCorrectAnswer(data.answer);
+  showCorrectAnswer(data.answer);
 
-    }
+}
+```
 
-  });
+};
 
 }
 
 function sendAnswer(index) {
 
-  socket.send(JSON.stringify({
-    type: "quiz_answer",
-    answer: index
-  }));
+socket.send(JSON.stringify({
+type: "quiz_answer",
+answer: index
+}));
 
 }
