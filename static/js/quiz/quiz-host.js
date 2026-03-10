26852ebdc016d40ba441361d6ab0c7eefdca67e4
AxEureka <lilium813@gmail.com>
2026-03-10 10:48:41 +0900

    Update quiz-host.js

--- static/js/quiz/quiz-host.js
+++ static/js/quiz/quiz-host.js
@@ -46,15 +46,9 @@ socket.addEventListener("message", e=>{
 
 const data = JSON.parse(e.data);
 
-if(data.type === "quiz_answer"){
+if(data.type === "quiz_votes"){
 
-const a = data.answer;
-if(a === undefined) return;
-
-votes[a]++;
-
-broadcastVotes();
-updateVotes();
+updateVotesFromServer(data.votes);
 
 }
 
@@ -90,17 +84,15 @@ function revealAnswer(){
 
 socket.send(JSON.stringify({
 type:"quiz_correct",
-answer:correctAnswer
+correct:correctAnswer
 }));
 
 }
 
-function broadcastVotes(){
+function updateVotesFromServer(serverVotes){
 
-socket.send(JSON.stringify({
-type:"quiz_votes",
-votes:votes
-}));
+votes = serverVotes;
+updateVotes();
 
 }
 
