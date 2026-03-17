15cccd7a6ce36f6f362fe1b270a31d995b92bdda
AxEureka <lilium813@gmail.com>
2026-03-17 11:17:19 +0900

    Update nasa-host.js

--- static/js/nasa/nasa-host.js
+++ static/js/nasa/nasa-host.js
@@ -1,21 +1,18 @@
-import {createItemEditor} from "./nasa-ui.js";
+import { createItemEditor } from "./nasa-ui.js";
 
 let socket;
 let container;
 
-export function startNASAHost(ws,uiContainer){
+export function startNASAHost(ws, uiContainer) {
+  socket = ws;
+  container = uiContainer;
 
-socket=ws;
-container=uiContainer;
-
-createItemEditor(container,(items,correct)=>{
-
-socket.send(JSON.stringify({
-type:"nasa_start",
-items:items,
-correct:correct
-}));
-
-});
-
+  // 親画面で品目と正解設定
+  createItemEditor(container, (items, correct) => {
+    socket.send(JSON.stringify({
+      type: "nasa_start",
+      items: items,
+      correct: correct
+    }));
+  });
 }
