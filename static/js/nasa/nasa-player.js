fa3bd26986aeae07e22d4645c002e1dcc86b5e3a
AxEureka <lilium813@gmail.com>
2026-03-17 12:45:24 +0900

    Update nasa-player.js

--- static/js/nasa/nasa-player.js
+++ static/js/nasa/nasa-player.js
@@ -1,116 +1,75 @@
-import { createRankingUI, showCorrect, showScore } from "./nasa-ui.js";
+import { createRankingUI, showCorrect, showRanking } from "./nasa-ui.js";
 
 let socket;
 let container;
 
-let items = [];
-let correct = [];
-let personal = [];
-let team = [];
+let items=[];
+let personal=[];
+let team=[];
+let teamName="";
 
-let teamName = "";
+export function startNASAPlayer(ws,uiContainer){
 
-export function startNASAPlayer(ws, uiContainer) {
+socket=ws;
+container=uiContainer;
 
-socket = ws;
-container = uiContainer;
-
-socket.addEventListener("message", (e) => {
+socket.addEventListener("message",(e)=>{
 
 let data;
-try { data = JSON.parse(e.data); } catch { return; }
+try{data=JSON.parse(e.data);}catch{return;}
 
-if (data.type === "nasa_start") {
-
-items = data.items;
-correct = data.correct;
-
+if(data.type==="nasa_start"){
+items=data.items;
 startPersonal();
-
 }
 
-if (data.type === "nasa_result") {
+if(data.type==="nasa_result"){
+showCorrect(container,items,data.correct,()=>{
+socket.send(JSON.stringify({type:"nasa_get_ranking"}));
+});
+}
 
-showCorrect(container, items, data.correct);
-showScore(container, calcScore(data.answers), calcScore(data.correct));
-
+if(data.type==="nasa_ranking"){
+showRanking(container,data,false);
 }
 
 });
 
 }
 
-
 function startPersonal(){
 
-createRankingUI(
-container,
-items,
-(r)=>{
+createRankingUI(container,items,(r)=>{
 
-personal = r;
+personal=r;
 
 socket.send(JSON.stringify({
 type:"nasa_personal",
+name:window.myName,
 ranks:r
 }));
 
 startTeam();
 
-},
-`${window.myName} の回答`
-);
+},`${window.myName} の回答`);
 
 }
-
 
 function startTeam(){
 
-teamName = prompt("チーム名を入力してください","チームA") || "チーム";
+teamName=prompt("チーム名","チームA")||"チーム";
 
-createRankingUI(
-container,
-items,
-(r)=>{
+createRankingUI(container,items,(r)=>{
 
-team = r;
+team=r;
 
 socket.send(JSON.stringify({
 type:"nasa_team",
+name:window.myName,
+team:teamName,
 ranks:r
 }));
 
-showResult();
-
-},
-`${teamName} の回答`
-);
-
-}
-
-
-function calcScore(answer){
-
-let score=0;
-
-for(let i=0;i<answer.length;i++){
-
-const c = correct && correct[i] !== undefined ? correct[i] : 0;
-
-score += Math.abs(answer[i] - c);
-
-}
-
-return score;
-
-}
-
-
-function showResult(){
-
-const p = calcScore(personal);
-const t = calcScore(team);
-
-showScore(container,p,t);
+},`${teamName} の回答`);
 
 }
