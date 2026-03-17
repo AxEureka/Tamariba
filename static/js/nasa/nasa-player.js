d16784b4c0c9d6d2a6496de2107b0ac5533c3a77
AxEureka <lilium813@gmail.com>
2026-03-17 11:17:54 +0900

    Update nasa-player.js

--- static/js/nasa/nasa-player.js
+++ static/js/nasa/nasa-player.js
@@ -1,97 +1,68 @@
-import {
-createRankingUI,
-showCorrect,
-showScore
-} from "./nasa-ui.js";
+import { createRankingUI, showCorrect, showScore } from "./nasa-ui.js";
 
 let socket;
 let container;
+let items = [];
+let correct = [];
+let personal = [];
+let team = [];
 
-let items=[];
-let correct=[];
-let personal=[];
-let team=[];
+export function startNASAPlayer(ws, uiContainer) {
+  socket = ws;
+  container = uiContainer;
 
-export function startNASAPlayer(ws,uiContainer){
+  socket.addEventListener("message", (e) => {
+    let data;
+    try { data = JSON.parse(e.data); } catch { return; }
 
-socket=ws;
-container=uiContainer;
+    if (data.type === "nasa_start") {
+      items = data.items;
+      correct = data.correct;
+      startPersonal();
+    }
 
-socket.addEventListener("message",(e)=>{
-
-let data;
-try{data=JSON.parse(e.data);}catch{return;}
-
-if(data.type==="nasa_start"){
-
-items=data.items;
-correct=data.correct;
-
-startPersonal();
-
+    if (data.type === "nasa_show_correct") {
+      showCorrect(container, items, correct);
+    }
+  });
 }
 
-if(data.type==="nasa_show_correct"){
+function startPersonal() {
+  createRankingUI(container, items, (r) => {
+    personal = r;
 
-showCorrect(container,items,correct);
+    socket.send(JSON.stringify({
+      type: "nasa_personal",
+      ranks: r
+    }));
 
+    startTeam();
+  });
 }
 
-});
+function startTeam() {
+  createRankingUI(container, items, (r) => {
+    team = r;
 
+    socket.send(JSON.stringify({
+      type: "nasa_team",
+      ranks: r
+    }));
+
+    showResult();
+  });
 }
 
-function startPersonal(){
-
-createRankingUI(container,items,(r)=>{
-
-personal=r;
-
-socket.send(JSON.stringify({
-type:"nasa_personal",
-ranks:r
-}));
-
-startTeam();
-
-});
-
+function calcScore(answer) {
+  let score = 0;
+  for (let i = 0; i < answer.length; i++) {
+    score += Math.abs(answer[i] - correct[i]);
+  }
+  return score;
 }
 
-function startTeam(){
-
-createRankingUI(container,items,(r)=>{
-
-team=r;
-
-socket.send(JSON.stringify({
-type:"nasa_team",
-ranks:r
-}));
-
-showResult();
-
-});
-
-}
-
-function calcScore(answer){
-
-let score=0;
-
-for(let i=0;i<answer.length;i++){
-score+=Math.abs(answer[i]-correct[i]);
-}
-
-return score;
-
-}
-
-function showResult(){
-
-const p=calcScore(personal);
-const t=calcScore(team);
-
-showScore(container,p,t);
-
+function showResult() {
+  const p = calcScore(personal);
+  const t = calcScore(team);
+  showScore(container, p, t);
 }
