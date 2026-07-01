af19eab426aef58a80f8fbc10ec7e9d7214edbce
AxEureka <lilium813@gmail.com>
2026-07-01 10:54:02 +0900

    Refactor startCompatibilityPlayer to use createCompatibilityUI

--- static/js/compatibility/compatibility-player.js
+++ static/js/compatibility/compatibility-player.js
@@ -1,3 +1,58 @@
-export function startCompatibilityPlayer(socket, container) {
-    console.log("compatibility player");
+import {
+    createCompatibilityUI
+}
+from "./compatibility-ui.js";
+
+export function startCompatibilityPlayer(
+    socket,
+    container
+){
+
+    socket.addEventListener(
+        "message",
+        (event)=>{
+
+            const data=
+                JSON.parse(event.data);
+
+            if(
+                data.type===
+                "start_compatibility"
+            ){
+
+                createCompatibilityUI(
+                    container,
+                    data.questions,
+                    (answers)=>{
+
+                        socket.send(
+                            JSON.stringify({
+
+                                type:
+                                "compatibility_answer",
+
+                                name:
+                                window.myName,
+
+                                answers:
+                                answers
+
+                            })
+                        );
+
+                        container.innerHTML=`
+                            <h2>
+                            回答を送信しました
+                            </h2>
+
+                            <p>
+                            他の参加者を
+                            待っています
+                            </p>
+                        `;
+                    }
+                );
+            }
+        }
+    );
 }
